01750ac27f0ae87b437dc07cd3435ddcd9bbdb66
spaezsuarez <spaezsuarez@gmail.com>
2021-01-19 09:32:28 -0500

    Creando pagina about(aun sin contenido)

--- src/view/app.js
+++ src/view/app.js
@@ -1,7 +1,7 @@
 //const main = require('electron').remote.require('./main') // bar
 const { remote } = require('electron');
 
-const Dao = remote.require('./lib/Dao');
+const homeController = remote.require('./controllers/homeController');
 const { parseString } = remote.require('./lib/proccesData');
 
 const form = document.getElementById('product-form');
@@ -11,17 +11,10 @@ const inputPrice = document.getElementById('price');
 const textArea = document.getElementById('description');
 
 
-form.addEventListener('submit',(event) => {
+
+form.addEventListener('submit', (event) => {
     event.preventDefault(); //Cancelar el comportamiento por defecto como lo es recargar la pagina
-
-    let newProduct = [Date.now(),parseString(inputName.value),parseFloat(inputPrice.value),parseString(textArea.value)];
-    let dao = new Dao();
-
-    dao.create('product',newProduct).then(() => {
-        
-    }).catch(() => {
-        
-    });
-
+    let newProduct = [Date.now(), parseString(inputName.value), parseFloat(inputPrice.value), parseString(textArea.value)];
+    homeController.addProduct('product',newProduct);
 
 });
