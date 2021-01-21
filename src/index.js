8b3f2a07df80be94b6531bcc70d0f2cf6e0c8400
spaezsuarez <spaezsuarez@gmail.com>
2021-01-21 09:47:26 -0500

    Remove electron reloader from the code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 const { createWindow } = require('./main');
 const { app } = require('electron'); //Objeto que maneja los eventos de la aplicacion
 
-require('electron-reload')(__dirname);
-
 app.allowRendererProcessReuse = false;
 app.whenReady().then(createWindow);
